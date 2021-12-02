91f7324c5267a952eed85c68b8a704fa7e8828c7
Kyle K. Lin <gazer.kanlin@gmail.com>
2021-12-02 09:28:54 +0800

    add clean method

--- lib/src/main/java/com/tragicphantom/stdf/RecordData.java
+++ lib/src/main/java/com/tragicphantom/stdf/RecordData.java
@@ -10,136 +10,151 @@
  *
  * Stdf4j is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with stdf4j.  If not, see <http://www.gnu.org/licenses/>.
-**/
+ * along with stdf4j. If not, see <http://www.gnu.org/licenses/>.
+ **/
 package com.tragicphantom.stdf;
 
-import java.util.HashMap;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Calendar;
-import java.util.TimeZone;
+import java.util.Collections;
+import java.util.HashMap;
 
-public class RecordData{
-   private RecordDescriptor desc;
-   private Object []        fields;
+public class RecordData {
 
-   // TODO: make configurable
-   //private static String dateFormat = "%1$tY-%1$tm-%1$tdT%1$tT";
-   private static String dateFormat = "%1$ta %1$tb %1$td %1$tT %1$tY";
+    private RecordDescriptor desc;
 
-   public RecordData(RecordDescriptor desc, Object [] fields){
-      this.desc   = desc;
-      this.fields = fields;
-   }
+    private Object[] fields;
 
-   public String getType(){
-      return desc.getType();
-   }
+    // TODO: make configurable
+    //private static String dateFormat = "%1$tY-%1$tm-%1$tdT%1$tT";
+    private static String dateFormat = "%1$ta %1$tb %1$td %1$tT %1$tY";
 
-   public int size(){
-      return fields.length;
-   }
+    public RecordData(RecordDescriptor desc, Object[] fields) {
+        this.desc = desc;
+        this.fields = fields;
+    }
 
-   public boolean hasField(String name){
-      return desc.contains(name);
-   }
+    public String getType() {
+        return this.desc.getType();
+    }
 
-   public void setField(int index, Object value){
-      fields[index] = value;
-   }
+    public int size() {
+        return this.fields.length;
+    }
 
-   public void setField(String name, Object value){
-      setField(desc.getIndex(name), value);
-   }
+    public boolean hasField(String name) {
+        return this.desc.contains(name);
+    }
 
-   public Object getField(String name){
-      return getField(desc.getIndex(name));
-   }
+    public void setField(int index, Object value) {
+        this.fields[index] = value;
+    }
 
-   public Object getField(int index){
-      return fields[index];
-   }
+    public void setField(String name, Object value) {
+        setField(this.desc.getIndex(name), value);
+    }
 
-   public HashMap<String, Object> getFields(){
-      HashMap<String, Object> values = new HashMap<String, Object>();
-      int index = 0;
-      for(Field field : desc.getFields())
-         values.put(field.getName(), fields[index++]);
-      return values;
-   }
+    public Object getField(String name) {
+        return getField(this.desc.getIndex(name));
+    }
 
-   public String getString(String name){
-      int    index = desc.getIndex(name);
-      Object value = getField(index);
-      char   type  = desc.getFields()[index].getType();
+    public Object getField(int index) {
+        return this.fields[index];
+    }
 
-      String repr;
-      if(value == null)
-         repr = "(null)";
-      else if(type == 'V' || type == 'k'){
-         StringBuilder lb = new StringBuilder();
-         for(Object item : (Object[])value){
-            if(lb.length() > 0)
-               lb.append(", ");
-            lb.append(item == null ? "(null)" : item.toString());
-         }
-         repr = lb.toString();
-      }
-      else if(value instanceof Double)
-         repr = String.format("%f", (Double)value);
-      else if(value instanceof Float)
-         repr = String.format("%f", (Float)value);
-      else if(value instanceof Byte)
-         repr = String.format("%x", (Byte)value);
-      else if(value instanceof byte[]){
-         StringBuilder bb = new StringBuilder("[");
-         for(byte b : (byte[])value){
-            if(bb.length() > 1)
-               bb.append(", ");
-            bb.append(Byte.toString(b));
-         }
-         bb.append("]");
-         repr = bb.toString();
-      }
-      else if((name.endsWith("_T") && !name.equals("TEST_T"))
-              || name.equals("MOD_TIM")){
-         Calendar cal = Calendar.getInstance();
-         //cal.setTimeZone(TimeZone.getTimeZone("GMT"));
-         cal.setTimeInMillis((Long)value * 1000L);
-         repr = String.format("%1$ta %1$tb %1$td %1$tT %1$tY", cal);
-      }
-      else
-         repr = value.toString();
+    public HashMap<String, Object> getFields() {
+        HashMap<String, Object> values = new HashMap<String, Object>();
+        int index = 0;
+        for (Field field : this.desc.getFields()) {
+            values.put(field.getName(), this.fields[index++]);
+        }
+        return values;
+    }
 
-      if(repr.length() == 0)
-         repr = "(null)";
+    public void clean() {
+        this.fields = new Object[0];
+    }
 
-      return repr;
-   }
+    public String getString(String name) {
+        int index = this.desc.getIndex(name);
+        Object value = getField(index);
+        char type = this.desc.getFields()[index].getType();
 
-   // this method has been tailored to output similar to libstdf format
-   // to help with comparing output of the two libraries
-   public String toString(){
-      StringBuilder sb = new StringBuilder(getType().toUpperCase());
-      sb.append("\n");
+        String repr;
+        if (value == null) {
+            repr = "(null)";
+        }
+        else if (type == 'V' || type == 'k') {
+            StringBuilder lb = new StringBuilder();
+            for (Object item : (Object[]) value) {
+                if (lb.length() > 0) {
+                    lb.append(", ");
+                }
+                lb.append(item == null ? "(null)" : item.toString());
+            }
+            repr = lb.toString();
+        }
+        else if (value instanceof Double) {
+            repr = String.format("%f", (Double) value);
+        }
+        else if (value instanceof Float) {
+            repr = String.format("%f", (Float) value);
+        }
+        else if (value instanceof Byte) {
+            repr = String.format("%x", (Byte) value);
+        }
+        else if (value instanceof byte[]) {
+            StringBuilder bb = new StringBuilder("[");
+            for (byte b : (byte[]) value) {
+                if (bb.length() > 1) {
+                    bb.append(", ");
+                }
+                bb.append(Byte.toString(b));
+            }
+            bb.append("]");
+            repr = bb.toString();
+        }
+        else if ((name.endsWith("_T") && !name.equals("TEST_T"))
+                || name.equals("MOD_TIM")) {
+            Calendar cal = Calendar.getInstance();
+            //cal.setTimeZone(TimeZone.getTimeZone("GMT"));
+            cal.setTimeInMillis((Long) value * 1000L);
+            repr = String.format("%1$ta %1$tb %1$td %1$tT %1$tY", cal);
+        }
+        else {
+            repr = value.toString();
+        }
 
-      // sort fields for easier comparisons
-      ArrayList<String> names = new ArrayList<String>(desc.getFieldNames());
-      Collections.sort(names);
+        if (repr.length() == 0) {
+            repr = "(null)";
+        }
 
-      for(String name : names){
-         sb.append("   ")
-           .append(name)
-           .append(": ")
-           .append(getString(name))
-           .append("\n");
-      }
+        return repr;
+    }
 
-      return sb.toString();
-   }
+    // this method has been tailored to output similar to libstdf format
+    // to help with comparing output of the two libraries
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(getType().toUpperCase());
+        sb.append("\n");
+
+        // sort fields for easier comparisons
+        ArrayList<String> names = new ArrayList<String>(this.desc.getFieldNames());
+        Collections.sort(names);
+
+        for (String name : names) {
+            sb.append("   ")
+                    .append(name)
+                    .append(": ")
+                    .append(getString(name))
+                    .append("\n");
+        }
+
+        return sb.toString();
+    }
 }
